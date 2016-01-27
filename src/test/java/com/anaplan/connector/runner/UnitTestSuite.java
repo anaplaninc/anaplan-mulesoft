d49320ebeab6ffdd978142db6e6747b2beab4121
Spondon Saha <spondon.saha@anaplan.com>
2016-01-26 17:59:44 -0800

    Added Import unit-tests to TestSuite

--- src/test/java/com/anaplan/connector/runner/UnitTestSuite.java
+++ src/test/java/com/anaplan/connector/runner/UnitTestSuite.java
@@ -1,13 +1,15 @@
 package com.anaplan.connector.runner;
 
 import com.anaplan.connector.unit.ConnectionUnitTestCases;
+import com.anaplan.connector.unit.ImportOperationTestCases;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-        ConnectionUnitTestCases.class})
+        ConnectionUnitTestCases.class,
+        ImportOperationTestCases.class})
 public class UnitTestSuite {
 
 }
